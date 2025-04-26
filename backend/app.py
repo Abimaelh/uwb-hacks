857d9b5d71863b684d5481c51c479765ce85429e
Sid T <sid.limitless@gmail.com>
2025-04-26 16:16:13 -0700

    Small fix to app.py

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,3 @@
-from flask import Flask
-from DBModel import db
-from routes import bp
-
-app = Flask(__name__)
-app.config.from_object('config.Config')
-
-db.init_app(app)
-app.register_blueprint(bp)
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
@@ -34,4 +21,4 @@ response = requests.get("https://v3.openstates.org/bills", headers=headers, para
 data = response.json()
 
 for bill in data.get("results", []):
-    print(f"{bill['identifier']}: {bill['title']}, {bill['classification']}")
+    print(f"{bill['identifier']}: {bill['title']}, {bill['subject']}")
