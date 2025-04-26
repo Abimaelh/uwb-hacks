42c8cfce223867fe0b45f3805af8f095d0f99a30
abimaelh <abimaelh@uw.edu>
2025-04-26 15:56:30 -0700

    learning react

--- frontend/my-landing-page/src/App.js
+++ frontend/my-landing-page/src/App.js
@@ -1,25 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+var showResults = false;
 
 function App() {
+    const [formData, setFormData] = useState({
+      message: ''
+    });
+    const[summaryData, setSummaryData] = useState({
+      summMessage: '',
+      source: ''
+    });
+  
+    const handleChange = (event) => {
+      const { name, value } = event.target;
+      setFormData(prevFormData => ({
+        ...prevFormData,
+        [name]: value
+      }));
+    };
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      window.alert('Form Data Submitted:'+ formData.message);
+      //back end placeholder logic here
+      //logic for converting the show results state
+    };
+
+  if(showResults) {
+    return (
+      <div className="App">
+      <p>{formData.message}</p>
+      <p>{summaryData.summMessage}</p>
+      <p>Sources</p>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>POLISEE</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="message">Message:</label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit">Submit</button>
+
+    </form>
       </header>
     </div>
   );
+
+  
 }
 
 export default App;
